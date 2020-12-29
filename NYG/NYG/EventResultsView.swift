f85a88b788e770939665125510bf5a302224a5ee
Komran Ghahremani <komranghahremani@Komrans-MBP.attlocal.net>
2020-12-29 16:33:20 -0500

    Event results view

--- NYG/NYG/EventResultsView.swift
+++ NYG/NYG/EventResultsView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct EventResultsView: View {
+    @State var isEditing: EditMode = .inactive
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(players, id: \.id) { player in
+                EventResultsRow(player: player)
+            }
+            .onMove(perform: move)
+        }
+        .environment(\.editMode, $isEditing)
+        .navigationBarTitle("Event Results")
+        .navigationBarItems(trailing: Button(action: {
+            self.isEditing = isEditing == .active ? .inactive : .active
+        }, label: {
+            Text("Adjust")
+        }))
+    }
+    
+    func move(from source: IndexSet, to destination: Int) {
+        players.move(fromOffsets: source, toOffset: destination)
     }
 }
 
 struct EventResultsView_Previews: PreviewProvider {
     static var previews: some View {
         EventResultsView()
+    }
+}
+
+struct EventResultsRow: View {
+    @State var points = NumbersOnly()
+    var player: Player = Player(name: "Komran")
+    
+    var body: some View {
+        HStack() {
+            Text(player.name)
+            TextField("Points", text: $points.value)
+                .multilineTextAlignment(.trailing)
+                .keyboardType(.numberPad)
+        }
+    }
+}
+
+final class NumbersOnly: ObservableObject {
+    @Published var value = "" {
+        didSet {
+            let filtered = value.filter { $0.isNumber }
+            
+            if value != filtered {
+                value = filtered
+            }
+        }
     }
 }
